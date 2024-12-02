d1ede4f24b511c75e9e9e714a9c7b4069220428e
rifal12330 <rifalmuhamad12330@gmail.com>
2024-12-03 00:24:52 +0700

    update commit

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const { connectDB } = require('./config/db');
+const { connectDB, sequelize } = require('./config/db'); // Mengimpor sequelize dan connectDB
+
 const {
   registerUser,
   loginUser,
