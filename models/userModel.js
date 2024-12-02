6623b20de27138d23e14effb3122327354712b31
rifal12330 <rifalmuhamad12330@gmail.com>
2024-12-02 15:54:54 +0700

    Pesan commit yang menjelaskan perubahan

--- models/userModel.js
+++ models/userModel.js
@@ -1,23 +1,31 @@
-const { Sequelize, DataTypes } = require('sequelize');
-const { sequelize } = require('../config/db');
-const bcrypt = require('bcrypt');
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database'); // Pastikan konfigurasi Sequelize Anda benar
 
 const User = sequelize.define('User', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false, // Nama wajib diisi
+    },
     email: {
         type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
+        allowNull: false, // Email wajib diisi
+        unique: true, // Email harus unik
+        validate: {
+            isEmail: true, // Validasi email
+        },
     },
     password: {
         type: DataTypes.STRING,
-        allowNull: false,
-    }
-});
-
-// Hook untuk enkripsi password sebelum menyimpan ke database
-User.beforeCreate(async (user) => {
-    const hashedPassword = await bcrypt.hash(user.password, 10);
-    user.password = hashedPassword;
+        allowNull: false, // Password wajib diisi
+    },
+}, {
+    tableName: 'users', // Nama tabel di database
+    timestamps: true, // Menambahkan kolom createdAt dan updatedAt
 });
 
 module.exports = User;
