438ef18bd262c03fca57c1a960d6b466458608e0
rifal12330 <rifalmuhamad12330@gmail.com>
2024-12-02 16:33:31 +0700

    Pesan commit yang menjelaskan perubahan

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,18 +1,20 @@
 const jwt = require('jsonwebtoken');
 
 const protect = (req, res, next) => {
-    const token = req.header('Authorization')?.replace('Bearer ', '');
-    if (!token) {
-        return res.status(401).json({ message: 'No token, authorization denied' });
-    }
+  const token = req.headers.authorization?.split(' ')[1];
 
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded;
-        next();
-    } catch (error) {
-        return res.status(401).json({ message: 'Token is not valid' });
-    }
+  if (!token) {
+    return res.status(401).json({ error: true, message: 'No token provided' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    console.error(error);
+    res.status(401).json({ error: true, message: 'Invalid token' });
+  }
 };
 
 module.exports = protect;
--- models/userModel.js
+++ models/userModel.js
@@ -1,33 +1,25 @@
 const { Sequelize, DataTypes } = require('sequelize');
 const { sequelize } = require('../config/db');
-const bcrypt = require('bcrypt');
 
 const User = sequelize.define('User', {
-    id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-    },
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: false,
-    },
-    email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-    },
-    password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    }
-});
-
-// Hook untuk enkripsi password sebelum menyimpan ke database
-User.beforeCreate(async (user) => {
-    const hashedPassword = await bcrypt.hash(user.password, 10);
-    user.password = hashedPassword;
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
 });
 
 module.exports = User;
