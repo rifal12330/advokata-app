3a4865aae8c76a5eda477b1488809ae79b49ffeb
rifal12330 <rifalmuhamad12330@gmail.com>
2024-12-04 00:24:30 +0700

    update commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 const { validationResult } = require('express-validator');
 const bcrypt = require('bcrypt');
-const User = require('../models/User'); // Sesuaikan dengan model User Anda
+const User = require('../models/userModel'); // Sesuaikan dengan model User Anda
 
 // Fungsi untuk menangani registrasi
 const registerUser = async (req, res) => {
