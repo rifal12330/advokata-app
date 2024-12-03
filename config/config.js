9329bbed63fb9ed92bf89c9b1529c6701da4d031
rifal12330 <rifalmuhamad12330@gmail.com>
2024-12-03 19:27:04 +0700

    update commit

--- config/config.js
+++ config/config.js
@@ -1,23 +1,23 @@
 module.exports = {
   "development": {
-    "username": "root", 
-    "password": "advokata123", 
+    "username": "rifal", 
+    "password": "1234", 
     "database": "advokataDB", 
     "host": "/cloudsql/amplified-coder-407814:asia-southeast2:advokata-db",
     "dialect": "mysql",
     "logging": false // 
   },
   "test": {
-    "username": "root", 
-    "password": "advokata123", 
+    "username": "rifal", 
+    "password": "1234", 
     "database": "advokataDB", 
     "host": "/cloudsql/amplified-coder-407814:asia-southeast2:advokata-db", 
     "dialect": "mysql",
     "logging": false
   },
   "production": {
-    "username": "root", 
-    "password": "advokata123",
+    "username": "rifal", 
+    "password": "1234",
     "database": "advokataDB",
     "host": "/cloudsql/amplified-coder-407814:asia-southeast2:advokata-db",
     "dialect": "mysql",
